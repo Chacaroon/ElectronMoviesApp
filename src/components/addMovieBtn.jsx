7b2b7499a1fdb695326c54573e6ccf78e4fc44a4
Chacaroon <tribunskiy.chacaroon@gmail.com>
2017-07-17 14:39:40 +0300

    Fix addMovieBtn

--- src/components/addMovieBtn.jsx
+++ src/components/addMovieBtn.jsx
@@ -1,9 +1,25 @@
 import React, {Component} from 'react' //eslint-disable-line
 import {Modal} from 'react-bootstrap' //eslint-disable-line
-import AddMovieForm from './addMovieForm.jsx' //eslint-disable-line
 import PropTypes from 'prop-types'
 
 export default class addMovieBtn extends Component { //parent Body
+
+    inputsHandle(event) {
+        const {name, value} = event.target
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    submitForm(e) {
+        const {title, description, rating, genre, year} = this.state
+        this.props.addMovie(title, description, rating, genre, year)
+        this.setState({
+            showModal: false
+        })
+        e.preventDefault()
+    }
 
     componentWillMount() {
         this.setState({
@@ -33,9 +49,39 @@ export default class addMovieBtn extends Component { //parent Body
                         <strong>Добавить фильм</strong>
                     </Modal.Header>
                     <Modal.Body>
-                        <AddMovieForm
-                            addMovie={::this.props.addMovie}
-                        />
+                        <form onSubmit={::this.submitForm} id="addMovieForm">
+                            <p>
+                                <label>
+                                    Название: <input type="text" name="title" onChange={::this.inputsHandle} required/>
+                                </label>
+                            </p>
+                            <p>
+                                <label>
+                                    Описание: <textarea type="text" name="description" rows="5" onChange={::this.inputsHandle} required/>
+                                </label>
+                            </p>
+                            <p>
+                                <label>Оценка:
+                                    <input type="number" name="rating" max="10" min="1" onChange={::this.inputsHandle} required/>
+                                </label>
+                            </p>
+                            <p>
+                                <label>
+                                    Жанр: <input type="text" name="genre" onChange={::this.inputsHandle} required/>
+                                </label>
+                            </p>
+                            <p>
+                                <label>
+                                    Год: <input type="number" name="year" min="1970" onChange={::this.inputsHandle} required/>
+                                </label>
+                            </p>
+                            <button type="submit">
+                                Отправить
+                            </button>
+                            <button type="button" onClick={::this.closeAddModal}>
+                                Закрыть
+                            </button>
+                        </form>
                     </Modal.Body>
                 </Modal>
             </div>
